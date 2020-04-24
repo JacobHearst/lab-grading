610d0c89db564d5a8ca7f4db8cb2d1d0562002bd
samuelsonj3712 <samuelsonj3712@my.uwstout.edu>
2020-04-24 14:30:59 -0500

    Updated grade_single_value to recieve post values

--- webpage/GradeLab/grade_single_value.php
+++ webpage/GradeLab/grade_single_value.php
@@ -1,3 +1,18 @@
+<?php
+
+	$lab_id = $_REQUEST["labID"];
+	$user_id = $_REQUEST["studentID"];
+	$lab_name = $_REQUEST["labName"];
+	$student_first_name = $_REQUEST["studentFirstName"];
+	$student_last_name = $_REQUEST["studentLastName"];
+	$section_id = $_REQUEST["sectionID"];
+	$servername = "144.13.22.59:3306";
+	$database = "G4AgileExperience";
+	$username = "g4AppUser";
+	$password = "aug4";
+
+?>
+
 <!doctype html>
 <html lang="en">
 	<head>
@@ -10,11 +25,13 @@
 
 	<body>
 		<h1>Grade Lab (Single Value)</h1>
+		<h2>Lab Name: <?=$lab_name?></h2>
+		<h2>Student Name: <?=$student_last_name?>, <?=$student_first_name?></h2>
+		<h2>Section ID: <?=$section_id?></h2>
 		<form name="gradeScore">
 			<fieldset>
 				<label for="score">Enter lab score: </label><br>
 				<input type="text" id="score" name="score">
-				
 				<button type="button" onClick="submitForm()">Submit</button>
 			</fieldset>
 		</form>
