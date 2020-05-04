2bb32c24630373195cc1bf6e3a3776a3e52185f2
Jacob Hearst <jhearst@protonmail.ch>
2020-05-04 09:38:09 -0700

    Fix search and database line length

--- webpage/database.php
+++ webpage/database.php
@@ -4,10 +4,18 @@ $username = "g4AppUser";
 $password = "aug4";
 $dbname = "G4AgileExperience";
 
+/**
+ * Get a database connection to execute queries with
+ */
 function getConnection() {
   global $servername, $username, $password, $dbname;
   try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, array(PDO::ATTR_PERSISTENT => true));
+    $conn = new PDO(
+        "mysql:host=$servername;dbname=$dbname",
+        $username,
+        $password,
+        array(PDO::ATTR_PERSISTENT => true)
+    );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $conn;
   }
@@ -17,6 +25,14 @@ function getConnection() {
   }
 }
 
+/**
+ * Execute a SQL query on the database using a prepared statement.
+ * Assumes that any parameters in the statement are replaced with
+ * question marks
+ * 
+ * @param {string} $query The SQL query
+ * @param {array} $params The parameters for the SQL statement
+ */
 function execQuery($query, $params) {
   $conn = getConnection();
   $stmt = $conn->prepare($query);
