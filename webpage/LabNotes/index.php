392a8ae077b410515d7aa418a2774491d60a3e54
TylerWanta <39772698+TylerWanta@users.noreply.github.com>
2020-05-05 19:44:49 -0500

    Updated notes search to work

--- webpage/LabNotes/index.php
+++ webpage/LabNotes/index.php
@@ -26,6 +26,10 @@ if (isset($_POST['note'])) {
   include '../database.php';
   execQuery($query, $params);
 }
+
+$selectQuery = 'SELECT * FROM Notes WHERE LabId=?';
+$notes = execQuery($selectQuery, $labId);
+
 ?>
 
 <!DOCTYPE html>
@@ -36,6 +40,7 @@ if (isset($_POST['note'])) {
       <link rel="stylesheet" type="text/css" href="LabNotes/style_labNotes.css">
   </head>
 
+<<<<<<< Updated upstream
   <body>
     <h1>Lab Grading Tool</h1>
     <h2>Programmed by: Agile Experience Group 4</h2>
@@ -55,6 +60,31 @@ if (isset($_POST['note'])) {
       <ul id="current-notes">
         <!-- Current notes go here. Doesn't need to be a list, that's just what I thought would work best-->
       </ul>
+=======
+	<body>
+		<h1>Lab Grading Tool</h1>
+		<h2>Programmed by: Agile Experience Group 4</h2>
+		<h4><a href="../">Return to main page.</a></h4>
+		
+		<br>
+		<hr>
+		
+        <h2>Add/Edit Lab Notes:</h2>
+        <form method="POST" id="add-note-form">
+            <label class="inline-block">Editing note:</label>
+            <!-- TODO: Update this field when a user clicks a button to edit a note -->
+            <input id="note-id" class="inline-block" name="noteId" type="text" disabled/>
+            <textarea class="block" name="note" rows=16 cols=32></textarea>
+            <button type=button onclick="resetNoteId()">Clear note ID</button>
+            <input type="submit" value="Submit"/>
+        </form>
+        <ul id="current-notes">
+			<?php 
+			foreach($notes as $note) { ?>
+				<li><?php $note['Note']?></li>
+			<?php } ?>
+        </ul>
+>>>>>>> Stashed changes
 
       <script src="LabNotes/main.js"></script>
   </body>
